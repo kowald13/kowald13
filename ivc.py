15e999062b9bcfceed11e2f4937a38a00ef287f7
kowald13 <93903526+kowald13@users.noreply.github.com>
2023-03-30 10:58:28 +0200

    Ujednolicenie projektu

--- ivc.py
+++ ivc.py
@@ -1,28 +1,82 @@
 #!C:\Users\dkowa\AppData\Local\Programs\Python\Python310\python.exe
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from datetime import datetime
 import os
 
+
+def header():
+    os.system('cls')
+    print("+-------------------------------------------------------------+")
+    print("|IVC - (Pomocnik sporządzania karty weryfikacji wskaźników)   |")
+    print("+-------------------------------------------------------------+")
+    print("|Oprogramowanie do tworzenia i ewidencjonowania ścieżek audytu|")
+    print("+-------------------------------------------------------------+")
+    print("|Dzisiaj jest: ", datetime.now().strftime('%d.%m.%Y'), "                                   |")
+    print("+-------------------------------------------------------------+")
+    return
+
+
 def zbierzdaneobiegudokumentow():
-    print ("Podaj dane ścieżki\n")
-    sprawa = input ("Sprawa: ")
-    dokument = input ("Dokument: ")
-    iddokumentu=sprawa+"("+dokument+")"
-    return(sprawa,dokument,iddokumentu)
+    print("\n")
+    sprawa = input("Sprawa: ")
+    dokument = input("Dokument: ")
+    return (sprawa, dokument)
+
+
+def importujdaneprojektu():
+    return
+
+
+def wprowadzdaneprojektu():
+    zbierzdaneobiegudokumentow()
+    return
+
+
+def nowyprojekt():
+    header()
+    czyimport = input("Czy zaimportować dane z pliku? (T/N)")
+    if czyimport.upper() == "T":
+        print("Wprowadz sygnatury dokumentu\n----------------------------");
+        importujdaneprojektu()
+    if czyimport.upper() == "N": wprowadzdaneprojektu()
+    return
+
+
+def listaprojektow():
+    return
+
 
 def nowasciezka():
+    header()
     print("Podaj dane nowej Karty weryfikacji")
+    zbierzdaneobiegudokumentow()
+
 
 def importujdanesciezki():
-    return()
+    return
 
-#main
-os.system("cls")
-nowasciezka()
-dane_sciezki=zbierzdaneobiegudokumentow()
-print (datetime.now().strftime('%d.%m.%Y %H:%M'))
-dzien=datetime.now().strftime('%d')
-print ("Sprawa: ",dane_sciezki[0])
-print ("Dokument: ",dane_sciezki[1])
-print ("Identfikator dokumentu:",dane_sciezki[2])
-print ("Dzisiaj jest",dzien,"dzień miesiąca")
+
+def menuglowne():
+    # wyświetlanie menu
+    print("[N]owy projekt | [I]stniejący projekt")
+
+    # obsługa menu
+    opcja = input("Wybierz opcję i naciśnij Enter")
+    if opcja.upper() == "N": nowyprojekt()
+    if opcja.upper() == "I":
+        listaprojektow()
+    else:
+        start()
+    return
+
+
+def start():
+    header()
+    menuglowne()
+
+    return
+
+
+# main
+
+start()
